80575cc65188c7de9847bea8a451d41b9f3c3cc1
Владислав <vladislav@MacBook-Pro-Vladislav.local>
2020-11-30 04:26:17 +0300

    version-1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+namespace консоль
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("test");
+        }
+
+       
+    }
+}
